e8a106e392d45a733892656bc642e92f8eba8a72
Jeongtae Kim <rlawjdxo1212@naver.com>
2020-04-06 22:54:47 +0900

    [Implement] Update slider and calc button

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func onHeightChanged(_ sender: UISlider) {
+        let value = String(format : "%.2f", sender.value)
+        heightLabel.text = "\(value)m"
 
+    }
+    @IBAction func onWeightChanged(_ sender: UISlider) {
+        let value = Int(sender.value)
+        weightLabel.text = "\(value)kg"
+
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
 }
 
